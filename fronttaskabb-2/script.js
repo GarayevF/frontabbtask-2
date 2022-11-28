a777f0eda7797aa6e0f7b8ee477531603d4a0316
GarayevF <112488747+GarayevF@users.noreply.github.com>
2022-11-28 19:23:40 +0400

    Add files via upload

---
+++ fronttaskabb-2/script.js
@@ -0,0 +1,16 @@
+let axtarish = document.querySelector('#axtarish');
+let close = document.querySelector('#closeicon');
+let lang = document.querySelector('#lang');
+
+axtarish.addEventListener('click', function() {
+    document.querySelector('#Search').classList.toggle('active-1');
+})
+
+close.addEventListener('click', function() {
+    document.querySelector('#Search').classList.remove('active-1')
+})
+
+
+lang.addEventListener('click', function(){
+    document.querySelector('#langdiv').classList.toggle('active-1')
+})
