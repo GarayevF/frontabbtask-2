d1535f2edbc3272d7a8a3c42d9b957aeaacb2b0e
GarayevF <112488747+GarayevF@users.noreply.github.com>
2022-11-29 18:38:25 +0400

    Add files via upload

--- fronttaskabb-2/script.js
+++ fronttaskabb-2/script.js
@@ -1,17 +1,67 @@
-let axtarish = document.querySelector('#axtarish');
-let close = document.querySelector('#closeicon');
-let lang = document.querySelector('#lang');
+// TASK 1
 
-axtarish.addEventListener('click', function() {
-    document.querySelector('#Search').classList.toggle('active-1');
-})
+// let axtarish = document.querySelector('#axtarish');
+// let close = document.querySelector('#closeicon');
+// let lang = document.querySelector('#lang');
 
-close.addEventListener('click', function() {
-    document.querySelector('#Search').classList.remove('active-1')
-})
+// axtarish.addEventListener('click', function() {
+//     document.querySelector('#Search').classList.toggle('active-1');
+// })
+
+// close.addEventListener('click', function() {
+//     document.querySelector('#Search').classList.remove('active-1')
+// })
 
 
-lang.addEventListener('click', function(){
-    document.querySelector('#langdiv').classList.toggle('active-1')
-    document.querySelector('#langicon').classList.toggle('active-2')
-})
+// lang.addEventListener('click', function(){
+//     document.querySelector('#langdiv').classList.toggle('active-1')
+//     document.querySelector('#langicon').classList.toggle('active-2')
+// })
+
+
+/////////////////////////////////////////////////
+
+
+// TASK 2
+
+// let square = document.querySelector('#square')
+// let circle = document.querySelector('#circle')
+
+// let wdth;
+// let hgth;
+
+// let topY = 0;
+// let leftX = 0;
+
+// document.addEventListener('keydown', function(e){
+//     wdth = window.innerWidth;
+//     hgth = window.innerHeight;
+
+//     if (e.key === 'ArrowRight' && leftX <= wdth-100) {
+//         leftX += 30;
+//         square.style.left = leftX+'px'
+//     }else if (e.key === 'ArrowLeft' && leftX >= 30) {
+//         leftX -= 30;
+//         square.style.left = leftX+'px'
+//     }else if (e.key === 'ArrowUp' && topY >= 30) {
+//         topY -= 30;
+//         square.style.top = topY+'px'
+//     }else if (e.key === 'ArrowDown' && topY <= hgth - 100) {
+//         topY += 30;
+//         square.style.top = topY+'px'
+//     }
+
+//     if (leftX > wdth/2-200 && leftX < wdth/2+100 && topY > hgth/2-200 && topY < hgth/2+100) {
+//         circle.style.backgroundColor = 'green'
+//     } else {
+//         circle.style.backgroundColor = 'red'
+//     }
+
+// })
+
+
+
+
+
+
+
